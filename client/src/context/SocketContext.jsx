11ff35661af031cc75e27c27b235e13cf8e5ea8e
Dishank <82960196+toyouin2000@users.noreply.github.com>
2026-06-28 21:26:50 +0530

    Change server URL to production endpoint

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 
 const SocketContext = createContext(null);
 
-const SERVER_URL = 'http://localhost:3001';
+const SERVER_URL = 'https://coup-m28v.onrender.com';
 
 export function SocketProvider({ children }) {
   const socketRef = useRef(null);
